d3e03f89590a1fa5f3058e20c00ec75bfa1e902a
cpatton <cpatton@palantir.com>
2014-01-18 22:35:48 -0800

    Modified the bro add script to be more descriptive.  Now instructs to add just one example

--- lib/bro.rb
+++ lib/bro.rb
@@ -124,7 +124,7 @@ command :add do |c|
       if cmd.nil?
         say "\nYou must enter a COMMAND after 'bro add'. For example: bro add curl\n\n"
       else
-        prompt = "#~ Bro entry for command '#{cmd}'\n#~ Just provide a few, simple, common case examples for how to use this command\n#~ Comments starting with #~ are removed\n"
+        prompt = "#~ Bro entry for command '#{cmd}'\n#~ Provide a useful example for how to use '#{cmd}'\n#~ Comments starting with #~ are removed\n\n#~ Example for command 'man':\n#~ #Opens up the manual page for the command 'ls'\n#~ man ls\n\n#~ Remove leading #~ when done:\n#~ #[INSERT DESCRIPTION OF EXAMPLE HERE]\n#~ [EXAMPLE HERE]"
         entry = ask_editor prompt, "vim"
         if entry.gsub(prompt, '').strip.length > 0
           if agree "Submit this entry for #{cmd}? [Yn] "
