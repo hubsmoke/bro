29698162ebbddc386aec08d33b06281dd1d07241
Ben Biddington <ben.biddington@gmail.com>
2014-01-26 13:46:44 +1300

    Refactor: relaxed assertion

--- spec/system.tests/examples.rb
+++ spec/system.tests/examples.rb
@@ -4,9 +4,10 @@ describe "Basic examples" do
   it "can ask about curl for example" do
     result = ColourBlind.strip(BroCli.run "curl")
 
-    expect(result).to match /[\d+] entries for curl -- submit your own example with "bro add curl"/
+    expect(result).to match /[\d+] entries for curl/
   end
 
+  it "rejects an unknown command"
   it "you can turn color off"
 end
 
