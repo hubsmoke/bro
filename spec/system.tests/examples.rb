e741b1abf0228ad6f677de8065ccb2f3eec76749
Ben Biddington <ben.biddington@gmail.com>
2014-01-26 13:54:51 +1300

    tells me when there is no manual for a command

--- spec/system.tests/examples.rb
+++ spec/system.tests/examples.rb
@@ -15,7 +15,14 @@ describe "Basic examples" do
     expect(result).to match /[\d+] entries for curl/
   end
 
-  it "rejects an unknown command"
+  it "tells me when there is no manual for a command" do
+    an_unknown_command = "xxx_unknown_command_xxx" 
+
+    result = bro an_unknown_command 
+
+    expect(result).to match /The #{an_unknown_command} command isn't in our database/
+  end
+
   it "you can turn color off"
 end
 
